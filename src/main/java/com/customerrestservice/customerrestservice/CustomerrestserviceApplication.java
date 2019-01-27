137e474461e09f07c6c00d9b667eba8738c1d5ff
James Collerton <jamescollerton@live.co.uk>
2019-01-27 14:23:45 +0000

    Initial commit

---
+++ src/main/java/com/customerrestservice/customerrestservice/CustomerrestserviceApplication.java
@@ -0,0 +1,14 @@
+package com.customerrestservice.customerrestservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerrestserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerrestserviceApplication.class, args);
+	}
+
+}
+
